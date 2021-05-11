db07996d1a39b343ec7fe15a048be080d81d0876
Samuel Roth <samuel6roth@gmail.com>
2021-05-11 17:12:04 -0500

    Working on blog page redirect

--- models/index.js
+++ models/index.js
@@ -1,4 +1,16 @@
 const User = require('./User');
+const Blog = require('./Blog')
 
-module.exports = { User };
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+  Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+
+
+module.exports = { User, Blog };
 
