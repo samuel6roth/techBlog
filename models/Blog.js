aa38598bdc95d8100b4624ba7ec42be4f8d0e330
Samuel Roth <samuel6roth@gmail.com>
2021-05-17 18:09:00 -0500

    Got seeds working

--- models/Blog.js
+++ models/Blog.js
@@ -11,21 +11,17 @@ Blog.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    post_title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    description: {
+    content: {
       type: DataTypes.STRING,
     },
     date_created: {
       type: DataTypes.DATE,
       allowNull: false,
       defaultValue: DataTypes.NOW,
-    },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
     },
     user_id: {
       type: DataTypes.INTEGER,
